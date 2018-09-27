4bcc8d7b1f76b2795c3a054ae1ed5e3d8019bed4
hoj201 <hoj201@gmail.com>
2018-09-27 16:47:00 -0400

    forgot to strip \n from the uuid string

--- batch_extract_relevant_gifs.py
+++ batch_extract_relevant_gifs.py
@@ -2,6 +2,7 @@ import sys
 from subprocess import run, PIPE
 with open(sys.argv[1],'r') as f:
     for uuid in f.readlines():
+        uuid = uuid.strip('\n')
         cmd = 'sh extract_relevant_gifs.sh {:s}'.format(uuid)
         completed_process = run(cmd,shell=True, stdout=PIPE, stderr=PIPE, encoding='utf-8')
         with open('logs/{:s}.log'.format(uuid),'w') as flog:
